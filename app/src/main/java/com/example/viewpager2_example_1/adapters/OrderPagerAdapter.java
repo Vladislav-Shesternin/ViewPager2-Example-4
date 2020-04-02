c1bbe6c2508857ace70bc9d7a5b909134589864a
Vlad-Shestenrin <valdon1202@gmail.com>
2020-04-02 16:17:04 +0300

    ViewPager2-Example-1

---
+++ app/src/main/java/com/example/viewpager2_example_1/adapters/OrderPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.viewpager2_example_1.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.viewpager2_example_1.fragments.ConfirmedOrdersFragment;
+import com.example.viewpager2_example_1.fragments.DeliveredOrdersFragment;
+import com.example.viewpager2_example_1.fragments.PendingOrdersFragment;
+
+public class OrderPagerAdapter extends FragmentStateAdapter {
+
+    public OrderPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = new PendingOrdersFragment();
+                break;
+            case 1:
+                fragment = new ConfirmedOrdersFragment();
+                break;
+            case 2:
+                fragment = new DeliveredOrdersFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/viewpager2_example_1/fragments/ConfirmedOrdersFragment.java
@@ -0,0 +1,30 @@
+package com.example.viewpager2_example_1.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.viewpager2_example_1.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ConfirmedOrdersFragment extends Fragment {
+
+    public ConfirmedOrdersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmed_orders, container, false);
+    }
+}
